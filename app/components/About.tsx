5872b2a24202846b3d367c09025f94cf371ce020
zuikeviciute <zuikeviciute@gmail.com>
2024-04-13 02:39:40 +0100

    replacing temporary content

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,7 +5,7 @@ export function About() {
         
         <div className="col-span-7">
           <h1 className="text-3xl text-white px-4 italic font-bold">Hi! I'm Laura</h1>
-          <p className="text-md text-white p-4 text-wrap">Recent Computer Science graduate based in UK. Currently working on personal projects
+          <p className="text-md text-white p-4 text-wrap">Currently working on personal projects
             and looking to join a team as a junior web developer! C# has been my go to language for about 3 years now and I’ve used it to make desktop apps, web apps and games.</p>
         </div>
         {/* <img src={"laura.png"} alt="Photo" className="object-cover w-full h-fit right-2 rounded-lg col-span-2"/> */}
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -6,20 +6,20 @@ export function Projects() {
     <div className="py-4 mx-6">
       <div className="max-w-3xl mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
         
-        <div className="relative col-span-3 order-1">
+        <div className="relative col-span-3 lg:col-span-2 order-1">
           <a href={"https://github.com/zuikeviciute/portfolio-web"}>
-                <img src={"/static/images/unity1.png"} alt="project image" className="object-cover h-56 w-full border-[#191919] border-2 rounded-lg hover:outline hover:outline-4 hover:outline-offset-0 outline-[#F17C60]" />
-                <div className={"absolute left-2 bottom-1 flex flex-row justify-center items-center text-center text-sm font-medium bg-[#191919] p-1 rounded-lg text-white"}>
-                    <div className={"mx-2"}>{"Project 1"}</div>
+                <img src={"/static/images/portfolio_img.png"} alt="project image" className="object-cover h-56 w-full border-[#191919] border-2 rounded-lg hover:outline hover:outline-4 hover:outline-offset-0 outline-[#F56441]" />
+                <div className={"absolute left-2 bottom-2 flex flex-row justify-center items-center text-center text-sm font-medium bg-[#191919] p-1 rounded-lg text-white"}>
+                    <div className={"mx-2"}>{"Portfolio Website"}</div>
                 </div>
             </a>
         </div>
 
-        <div className="relative lg:col-span-2 order-2">
+        <div className="relative col-span-3 lg:col-span-3 order-2">
           <a href={"https://github.com/zuikeviciute/portfolio-web"}>
-                <img src={"/static/images/uxui2.jpg"} alt="project image" className="object-cover h-56 w-full border-[#191919] border-2 rounded-lg hover:outline hover:outline-4 hover:outline-offset-0 outline-[#F17C60]" />
-                <div className={"absolute left-2 bottom-1 flex flex-row justify-center items-center text-center text-sm font-medium bg-[#191919] p-1 rounded-lg text-white"}>
-                    <div className={"mx-2"}>{"Project 2"}</div>
+                <img src={"/static/images/maupak.png"} alt="project image" className="object-cover h-56 w-full border-[#191919] border-2 rounded-lg hover:outline hover:outline-4 hover:outline-offset-0 outline-[#FAA743]" />
+                <div className={"absolute left-2 bottom-2 flex flex-row justify-center items-center text-center text-sm font-medium bg-[#191919] p-1 rounded-lg text-white"}>
+                    <div className={"mx-2"}>{"Smart Projector App Design"}</div>
                 </div>
             </a>
         </div>
@@ -33,22 +33,22 @@ export function Projects() {
         </div>
 
         <div className="relative col-span-3 order-4">
-          <a href={"https://github.com/zuikeviciute/portfolio-web"}>
-                <img src={"uxui3.png"} alt="project image" className="object-cover h-56 w-full border-[#191919] border-2 rounded-lg hover:outline hover:outline-4 hover:outline-offset-0 outline-[#BBC842]" />
-                <div className={"absolute left-2 bottom-1 flex flex-row justify-center items-center text-center text-sm font-medium bg-[#191919] p-1 rounded-lg text-white"}>
-                    <div className={"mx-2"}>{"Project 3"}</div>
+          <a href={"https://github.com/zuikeviciute/3D-Serious-Game"}>
+                <img src={"/static/images/unity.png"} alt="project image" className="object-cover h-56 w-full border-[#191919] border-2 rounded-lg hover:outline hover:outline-4 hover:outline-offset-0 outline-[#BBC842]" />
+                <div className={"absolute left-2 bottom-2 flex flex-row justify-center items-center text-center text-sm font-medium bg-[#191919] p-1 rounded-lg text-white"}>
+                    <div className={"mx-2"}>{"3D Unity Game"}</div>
                 </div>
             </a>
         </div>
-
+{/* 
         <div className="relative lg:col-span-2 col-span-3 order-5">
-          <a href={"https://github.com/zuikeviciute/portfolio-web"}>
-                <img src={"/db1.png"} alt="project image" className="object-cover h-56 w-full border-[#191919] border-2 rounded-lg hover:outline hover:outline-4 hover:outline-offset-0 outline-[#BBC842]" />
-                <div className={"absolute left-2 bottom-1 flex flex-row justify-center items-center text-center text-sm font-medium bg-[#191919] p-1 rounded-lg text-white"}>
-                    <div className={"mx-2"}>{"Project 4"}</div>
+          <a href={"https://zuikeviciute.vercel.app/"}>
+                <img src={"/static/images/more_projects.png"} alt="project image" className="object-cover h-56 w-full border-[#191919] border-2 rounded-lg hover:outline hover:outline-4 hover:outline-offset-0 outline-[#BBC842]" />
+                <div className={"absolute left-2 bottom-2 flex flex-row justify-center items-center text-center text-sm font-medium bg-[#191919] p-1 rounded-lg text-white"}>
+                    <div className={"mx-2"}>{"Other"}</div>
                 </div>
             </a>
-        </div>
+        </div> */}
       </div>
     </div>
   );
